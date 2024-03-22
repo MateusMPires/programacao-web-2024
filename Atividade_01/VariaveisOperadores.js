ea1afd882d4a2acaae08fbdb00a22a2ae0bc866f
MateusMPires <mateusmp.df@gmail.com>
2024-03-22 10:22:58 -0300

    Atividade 1 Feita

---
+++ Atividade_01/VariaveisOperadores.js
@@ -0,0 +1,15 @@
+// Declaração das variáveis numéricas e inicialização
+let a = 10;
+let b = 5;
+
+// Operações
+let soma = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+
+// Impressão dos resultados
+console.log(`Resultado da soma entre ${a} e ${b}: ${soma}`);
+console.log(`Resultado da subtração entre ${a} e ${b}: ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${a} e ${b}: ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${a} e ${b}: ${divisao}`);
